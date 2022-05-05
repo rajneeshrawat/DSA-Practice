9d23e36351bce401b04c6b68a45bf11ffc9e1251
rajneeshrawat <104812920+rajneeshrawat@users.noreply.github.com>
2022-05-06 00:13:13 +0530

    Create pointer with variable.cpp

---
+++ assignment 3/pointer with variable.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    int sum = *a + *b;
+    int sub =*a -*b ;
+    *a =sum;
+    if(sub<0)
+    {
+        sub=-sub;
+    }
+    *b = sub ;
+    
+    
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
