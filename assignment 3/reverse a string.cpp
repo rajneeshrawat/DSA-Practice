b0f609617591da42795cb1bcacdddbb673b93fd0
rajneeshrawat <104812920+rajneeshrawat@users.noreply.github.com>
2022-05-05 09:37:26 +0530

    Create reverse a string.cpp

---
+++ assignment 3/reverse a string.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string a;
+    cin>> a;
+    int n=a.size();
+    for(int i=0;i<=n;i++)
+    {
+        cout<<a[n-i];
+    }
+    return 0;
+    
+}
