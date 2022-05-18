ea4aa3f915baa683d766a61d75897ac3a45a3a1b
rajneeshrawat <104812920+rajneeshrawat@users.noreply.github.com>
2022-05-18 18:49:58 +0530

    Update prime.cpp

--- assignment 2/prime.cpp
+++ assignment 2/prime.cpp
@@ -2,26 +2,24 @@
 using namespace std;
 
 bool isPrime(int n){
-    if(n<=1){
-       return 0; 
-    }
-    else
-    for (int i=2;i<sqrt(n);i++){
-      if (n%i==0){
-            return 0;
-        }
-    }
-    return 1;
-    
+   int c=1;
+   for(int i=2;i<=sqrt(n);i++)
+   {
+      if(n%i == 0)
+      {
+         c = 0;
+         break;
+      }
+   }
+    return c;
 }
 int main() {
-    int p;
-    cin>>p;
-    if (isPrime(p)==0){
-        cout<<"not prime";
-        
-    }
-    else {
-        cout<<"prime";
+    for(int i=1;i<30;i++){
+         if (isPrime(i)){
+            cout<<"the number "<<i<<" is prime\n";
+         }
+         else{
+            cout<<"the number "<<i<<" is not prime\n";
+         }   
     }
 }
