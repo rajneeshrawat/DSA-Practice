207e2e47dcfa3fc8d6d71f923e6a2969401d4f8f
rajneeshrawat <104812920+rajneeshrawat@users.noreply.github.com>
2022-05-24 09:30:18 +0530

    Create adding two string.cpp

---
+++ C.W code/adding two string.cpp
@@ -0,0 +1,26 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    string add(string a, string b){\
+    	string sum = "";
+    	//complete the funcution;
+        return sum;
+    }
+    int main() {
+        if(add("123","234")!= "357"){
+            cout<<"wrong answer: 1\n";
+        }
+     
+        if(add("123","238")!= "361"){
+            cout<<"wrong answer: 2\n";
+        }
+     
+        if(add("123","888")!= "1011"){
+            cout<<"wrong answer: 3\n";
+        }
+     
+        if(add("123","88")!= "211"){
+            cout<<"wrong answer: 4\n";
+        }
+        std::cout << "Hello World!\n";
+    }
