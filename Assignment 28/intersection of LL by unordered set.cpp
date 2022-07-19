dffabe514899df67566cb4545a20c97399feda04
rajneeshrawat <104812920+rajneeshrawat@users.noreply.github.com>
2022-07-19 23:02:01 +0530

    Create intersection of LL by unordered set.cpp

---
+++ Assignment 28/intersection of LL by unordered set.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+        unordered_set<ListNode*>s;
+    
+        
+        while(headA!=NULL){
+            s.insert(headA);
+            headA= headA->next;
+            
+        }
+        
+        while (headB!=NULL){
+            if(s.find(headB)!=s.end()) return headB;
+            headB=headB->next;
+        }
+        return NULL;
+    }
+};
