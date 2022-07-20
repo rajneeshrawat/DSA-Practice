907f51d808a15421d0404303cbe3beaa29d72b6c
rajneeshrawat <104812920+rajneeshrawat@users.noreply.github.com>
2022-07-20 10:23:38 +0530

    Create count meals.cpp

---
+++ C.W code/count meals.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    long  long countPairsForTarget(vector <int> &A,int target) {
+        
+        unordered_map(int,int)m;
+        
+        int ans=0;
+        for (int a:A){
+            ans 
+            
+            
+        }    
+        
+    }
+        
+    int countPairs(vector<int>& deliciousness) {
+        
+        long long ans=0;
+        int t= 1;
+        
+        for(int i =1; i<=22; i++){
+            ans =(ans+ countPairsForTarget(A,t))%Mod;
+            
+            t*=2;
+        }
+        return ans;
+    }
+};
